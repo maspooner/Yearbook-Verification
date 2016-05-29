8b72e67c6eb475d99dfe8ea376e3c929afb06813
Matt Spooner <matt.spooner10@gmail.com>
2016-05-29 11:17:10 -0400

    Can compile a list of names from excel.

--- YearbookVerify/MainWindow.xaml.cs
+++ YearbookVerify/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using NetSpell.SpellChecker;
+﻿using Microsoft.Office.Interop.Excel;
+using NetSpell.SpellChecker;
 using NetSpell.SpellChecker.Dictionary;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,46 +12,95 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace YearbookVerify {
-	public partial class MainWindow : Window {
+	public partial class MainWindow : System.Windows.Window {
 		//members
 		private bool input;
 		private Spelling spellCheck;
 		private List<Name> names;
 		//constructors
 		public MainWindow() {
+			//TODO handle exceptions
 			InitializeComponent();
 			input = true;
 			spellCheck = new Spelling();
-			names = new List<Name>();
+			names = CompileMatchedList(); //TODO
 			WordDictionary wd = new WordDictionary();
 			wd.DictionaryFile = "registeredNames.dic";
-			wd.Add("cat");
-			wd.Add("dog");
-			wd.Add("cut");
 			spellCheck.Dictionary = wd;
 			spellCheck.SuggestionMode = Spelling.SuggestionEnum.PhoneticNearMiss;
-			spellCheck.Suggest("dig");
 			foreach (string str in spellCheck.Suggestions)
-				Console.WriteLine(str); 
+				Console.WriteLine(str);
+			File.WriteAllText("registeredNames.dic", Properties.Resources.dicBase + string.Join(Environment.NewLine, FindUnique()));
 		}
 		//methods
+		private List<Name> CompileMatchedList() {
+			List<Name> compNames = new List<Name>();
+			Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+			app.Visible = false;
+			Workbook wb = app.Workbooks.Open(@"F:\Documents\Visual Studio 2015\Projects\YearbookVerify\YearbookVerify\bin\Debug\input.xlsx");
+			Worksheet ws = wb.Worksheets[1];
+			string first, last;
+			int i = 3;
+			bool done;
+			do {
+				first = (string)ws.Cells[i, 2].Value;
+				last = (string)ws.Cells[i, 3].Value;
+				done = first == null || last == null || first.Length == 0 || last.Length == 0;
+				if (!done) {
+					compNames.Add(new Name(first, last));
+					Console.WriteLine(first + " " + last);
+					i++;
+				}
+            }
+			while (!done);
+			wb.Close(false);
+			app.Quit();
+			ReleaseInterop(ws);
+			ReleaseInterop(wb);
+			ReleaseInterop(app);
+			return compNames;
+		}
+		private List<string> FindUnique() {
+			List<string> unique = new List<string>();
+			foreach(Name n in names) {
+				if (!unique.Contains<string>(n.First))
+					unique.Add(n.First);
+				if (!unique.Contains<string>(n.Last))
+					unique.Add(n.Last);
+			}
+			foreach (string s in unique)
+				Console.WriteLine(s);
+			return unique;
+		}
+		private void ReleaseInterop(object obj) {
+			try {
+				System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+				obj = null;
+			}
+			catch (Exception ex) {
+				obj = null;
+				MessageBox.Show("Unable to release the Object " + ex.ToString());
+			}
+			finally {
+				GC.Collect();
+			}
+		}
 		private string FindSomeSuggestions(string word) {
 			spellCheck.Suggest(word);
 			List<string> suggs = new List<string>();
 			for (int i = 0; i < spellCheck.Suggestions.Count && i < 3; i++) {
-				suggs.Add(spellCheck.Suggestions[i] as string);
+				suggs.Add(CapFirst(spellCheck.Suggestions[i] as string));
 			}
 			string sugg = string.Join(" or ", suggs);
 			return sugg.Length > 0 ? " Did you mean: " + sugg + "?" : "";
 		}
 		private string MakeError(string s) {
 			return "***" + s + "***";
+		}
+		private string CapFirst(string s) {
+			return char.ToUpperInvariant(s[0]) + s.Substring(1).ToLowerInvariant();
 		}
 		private SpellingResult CheckSpelling(string text, char delim) {
 			string[] lines = text.Split(delim);
@@ -62,16 +113,18 @@ namespace YearbookVerify {
 				if (editLine.Length > 0) {
 					string[] words = editLine.Split(' ');
 					if(words.Length == 2) {
+						words[0] = CapFirst(words[0]);
+						words[1] = CapFirst(words[1]);
 						bool sFirst = spellCheck.TestWord(words[0]);
 						bool sLast = spellCheck.TestWord(words[1]);
 						if(sFirst && sLast) {
 							Name n = names.Find(name => name.First.Equals(words[0]) && name.Last.Equals(words[1]));
 							if(n == null) {
-								markedLines += MakeError(editLine) + " This name combination does not exist." + delim;
+								markedLines += MakeError(words[0] + " " + words[1]) + " This name combination does not exist." + delim;
 								noExist++;
 							}
 							else {
-								markedLines += editLine + delim;
+								markedLines += words[0] + " " + words[1] + delim;
                             }
 						}
 						else {
